1374e81366aa84801c31fcec31d348a50b84068e
harsha <harsha80956@gmail.com>
2023-05-06 06:13:11 +0530

    Responsive Navbar added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
+import tw from "twin.macro";
 import "./App.css";
+import styled from "styled-components";
+import { Home } from "./app/containers/Home";
+
+const AppContainer = styled.div`
+  ${tw`
+  w-full
+  h-full  
+  flex
+  flex-col
+  `}
+`;
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <AppContainer>
+      <Home />
+    </AppContainer>
+  );
 }
 
 export default App;
